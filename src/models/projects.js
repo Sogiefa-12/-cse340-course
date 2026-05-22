bbca3f8eaf2e682d81833881d18b941b2b1f2478
Sogiefa-12 <131720010+Sogiefa-12@users.noreply.github.com>
2026-05-22 15:43:02 +0100

    week activity

--- src/models/projects.js
+++ src/models/projects.js
@@ -1,6 +1,6 @@
 import db from './db.js';
 
-export async function getAllProjects() {
+const getAllProjects = async () => {
     const sql = `
         SELECT
             p.project_id,
@@ -17,3 +17,72 @@ export async function getAllProjects() {
     const result = await db.query(sql);
     return result.rows;
 }
+
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+            project_id,
+            organization_id,
+            title,
+            description,
+            location,
+            project_date
+        FROM projects
+        WHERE organization_id = $1
+        ORDER BY project_date;
+    `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+// Team Activity Week 3
+const getUpcomingProjects = async (number_of_projects) => {
+    const sql = `
+        SELECT 
+            p.project_id,
+            p.title,
+            p.description,
+            p.project_date,
+            p.location,
+            p.organization_id,
+            o.name AS organization_name
+        FROM projects p
+        JOIN organization o
+            ON p.organization_id = o.organization_id
+        WHERE p.project_date >= CURRENT_DATE
+        ORDER BY p.project_date ASC
+        LIMIT $1
+    `;
+
+    const result = await db.query(sql, [number_of_projects]);
+
+    return result.rows;
+};
+
+
+const getProjectDetails = async (id) => {
+    const sql = `
+        SELECT
+            p.project_id,
+            p.title,
+            p.description,
+            p.project_date,
+            p.location,
+            p.organization_id,
+            o.name AS organization_name
+        FROM projects p
+        JOIN organization o
+            ON p.organization_id = o.organization_id
+        WHERE p.project_id = $1
+    `;
+
+    const result = await db.query(sql, [id]);
+
+    return result.rows[0];
+};
+
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
